82430efdf8b17213a057692823432e5091df1186
hirano <hirano@tokyo-ict.com>
2025-06-03 20:58:10 +0900

    メッセージ追加

--- src/main/java/com/example/newdemo/NewdemoApplication.java
+++ src/main/java/com/example/newdemo/NewdemoApplication.java
@@ -9,6 +9,7 @@ public class NewdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewdemoApplication.class, args);
 		System.out.println("生理アプリ");
+		System.out.println("わーいわーい");
 	}
 
 }
